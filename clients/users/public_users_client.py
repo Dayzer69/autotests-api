92d9ec53b7984d15a2a74db6285d69733920f4e0
Dayzer69 <tischin.anton2011@yandex.ru>
2026-03-02 12:28:08 +0300

    6.3 Знакомство с api клиентами.Практика: Написание api клиента

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,27 @@
+from clients.api_client import APIClient
+from typing import TypedDict
+
+
+class CreateUserRequestDict(TypedDict):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
+class PublicUsersClient(APIClient):
+    """
+    Клиент для работы с /api/v1/users без авторизации
+    """
+    def create_user_api(self, request: CreateUserRequestDict):
+        """
+        Метод создаёт нового пользователя.
+
+        :param request: данные для создания пользователя.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.client.post('/api/v1/users', json=request)
