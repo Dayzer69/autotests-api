88e960d92fa0dc5ace955604cf0167f398c916f2
Dayzer69 <tischin.anton2011@yandex.ru>
2026-03-08 18:52:12 +0300

    8.5 Фикстуры ytest.Практика написания автотестов с использованием фикстур fixed

--- tests/test_users.py
+++ tests/test_users.py
@@ -38,3 +38,5 @@ def test_get_user_me(
         get_user_response=response_data.user,
         create_user_response=function_user.response
     )
+
+    validate_json_schema(response.json(), response_data.model_json_schema())
