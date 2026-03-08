7c0eba3c5dad6d65b917d6887fe3ab56eaadedee
Dayzer69 <tischin.anton2011@yandex.ru>
2026-03-08 17:43:40 +0300

    8.5 Фикстуры ytest.Практика написания автотестов с использованием фикстур

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,17 +1,14 @@
 from http import HTTPStatus
+
 import pytest
-from clients.users.public_users_client import get_public_users_client
+
+from clients.users.private_users_client import PrivateUsersClient
 from clients.users.public_users_client import PublicUsersClient
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from fixtures.users import UserFixture  # Заменяем импорт
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-from tools.assertions.users import assert_create_user_response
-from clients.users.private_users_client import PrivateUsersClient
-from clients.users.users_schema import GetUserResponseSchema
-from tests.conftest import UserFixture
-
-from tools.assertions.base import assert_status_code
-from tools.assertions.users import assert_get_user_response
+from tools.assertions.users import assert_create_user_response, assert_get_user_response
 
 
 @pytest.mark.authentication
