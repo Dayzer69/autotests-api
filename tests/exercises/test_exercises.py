0a25b75d0769be0b155d57b260afcdf4e68505f9
Dayzer69 <tischin.anton2011@yandex.ru>
2026-03-12 12:02:09 +0300

    9.4 рактикуемся в написании API автотестов. асть 3.рактика написания API автотестов для работы с заданиями. удаление задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -1,6 +1,7 @@
 import pytest
 from http import HTTPStatus
 
+from clients.errors_schema import InternalErrorResponseSchema
 from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
     GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
@@ -8,8 +9,9 @@ from fixtures.courses import CourseFixture
 from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
-    assert_update_exercise_response
+    assert_update_exercise_response, assert_exercise_not_found_response
 from tools.assertions.schema import validate_json_schema
+
 
 
 @pytest.mark.regression
@@ -55,3 +57,22 @@ class TestExercises:
             response_data
         )
 
+    def test_delete_exercise(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture):
+        exercise_id = function_exercise.response.exercise.id
+        response_delete = exercises_client.delete_exercise_api(
+            exercise_id=exercise_id
+        )
+
+        assert_status_code(response_delete.status_code, HTTPStatus.OK)
+
+        response_get = exercises_client.get_exercise_api(
+            exercise_id=exercise_id
+        )
+
+        assert_status_code(response_get.status_code, HTTPStatus.NOT_FOUND)
+
+        response_get_data = InternalErrorResponseSchema.model_validate_json(response_get.text)
+
+        assert_exercise_not_found_response(response_get_data)
+
+        validate_json_schema(response_get.json(), response_get_data.model_json_schema())
